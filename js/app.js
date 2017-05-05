3bc9c6fe07854385e31cba5f3ffb9b470fa1f635
Alex Aguinaldo <psionicbones@gmail.com>
2017-05-05 11:31:45 -1000

    Did clearEverything, remember to do clear and figure out why the function isn't functioning.

--- js/app.js
+++ js/app.js
@@ -13,7 +13,18 @@
   var handler = {
     //saves user inputs as one entry to local storage
     saveEntry: function (){
-      
+      var inputs = document.querySelectorAll(".tcell");
+      user.id = inputs[0].value;
+      user.name = inputs[1].value;
+      user.address = inputs[2].value;
+      user.email = inputs[3].value;
+
+      // Will convert obj into JSON and store in localstorage
+      // Grabs all le info, clicking button will call the method
+      localStorage.setItem("user_" + localStorage.length, JSON.stringify(user));
+
+      //reloads the page
+      location.reload();
     },
 
     //clears user input fields on the page
@@ -23,10 +34,31 @@
 
     //displays user entries
     displayEntry: function(){
-      
-    },
+      if (localStorage.length > 0){
+        var render = "<div>";
+        render += "<div id ='entry_container'>Entries:</div>";
+        for (i = 0; 1 < localStorage.length; i++){
+          // Gets key
+          var key = localStorage.key(i); 
+          // Gets data from key
+          var entry = localStorage.getItem(key);
+          // parses data back into object (everything is ultimately an object)
+          var data = JSON.parse(entry);
+          render += "<ul>";
+          render += "<li>" + data.id + "</li>";
+          render += "<li>" + data.name + "</li>";
+          render += "<li>" + data.address + "</li>";
+          render += "<li>" + data.email + "</li>";
+          render += "</ul>";
+        }
+        render += "</div>";
+        display_container.innerHTML = render;
+      }
+    },  
     
     clearEverything: function(){
+      // locationStorage.clear();
+      // location.relode();
     }
   };
 
